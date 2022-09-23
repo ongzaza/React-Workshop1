c499d42a6f2b601400ece3455b2f632b04c5a7e3
ongzaza <ong41544@gmail.com>
2022-09-23 11:52:25 +0700

    version1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createContext,useState } from 'react';
+import Menu from './component/Menu';
+import Quiz from './component/Quiz';
+import Score from './component/Score';
+
+import './App.css'
+
+export const DataContext = createContext()
 
 function App() {
+  const [appState, setAppState] = useState("menu")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataContext.Provider value={{appState,setAppState}}>
+      <div className="App">
+        <h1>Web Development Quiz</h1>
+          {appState === "menu" && <Menu/>} {}
+          {appState === "quiz" && <Quiz/>} {}
+          {appState === "score" && <Score/>} {}
+      </div>
+    </DataContext.Provider>
   );
 }
 
---
+++ src/component/Score.js
@@ -0,0 +1,8 @@
+const Score = ()=>{
+    return(
+        <div classname="quiz">
+            <h1>Score Component</h1>
+        </div>
+    )
+}
+export default Score;
